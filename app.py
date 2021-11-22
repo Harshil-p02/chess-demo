770ea3063cf1ede4811ae1e3b50b16846e646097
Prateek Kumar Jha <85632179+PKJ-x7@users.noreply.github.com>
2021-11-22 13:25:28 +0530

    Update app.py
    added move tokeniser function

--- app.py
+++ app.py
@@ -17,8 +17,36 @@ if "moves" not in st.session_state:
 
 if "cnt" not in st.session_state:
     st.session_state.cnt = 0
+    
+def tokeniser(pgn_move):
+    chars = list(pgn_move)
+
+    move = "[ "
+    if len(chars) == 2:
+        move += ''.join(chars) + " "
+    elif chars[0] == "O":
+        if len(chars) == 3 or len(chars) == 5:
+            move += ''.join(chars) + " "
+        else:
+            if len(chars) == 4:
+                move += "O-O " + chars[3] + " "
+            else:
+                move += "O-O-O " + chars[5] + " "
+    else:
+        j = 0
+        while j < len(chars):
+            if j + 1 < len(chars) and chars[j + 1].isnumeric():
+                move += ''.join(chars[j:j + 2]) + " "
+                j += 1
+            else:
+                move += chars[j] + " "
+            j += 1
+    move = move.strip()
+    move += " ]"
+    return move
 
 def run(move):
+    tokenised_move = tokeniser(move)
     if move is not None:
         st.session_state.game.push_san(move)
 
